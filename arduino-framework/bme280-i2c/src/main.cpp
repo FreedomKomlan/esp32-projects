0b95981f30d962c10041c9092fa4e1df016459bf
gds-tech <90794220+FreedomKomlan@users.noreply.github.com>
2026-04-22 14:12:23 +0100

    Commit on 2026-04-22 14:12: Initialize bme280-i2c project

--- arduino-framework/bme280-i2c/src/main.cpp
+++ arduino-framework/bme280-i2c/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define BME_ADDRESS 0xEC
+
+Adafruit_BME280 bme;
+
+float temp, pres, hum, alt;
+
+
+// Functions
+void getValues();
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("BME280 Test");
+
+  if (!(bme.begin(BME280_ADDRESS))) {
+    /* code */
+    Serial.println("BME280 not found");
+    while (1);
+    
+  }
+  Serial.println("BME280 Ready - Test");
+  delay(1000);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  getValues();
+  Serial.print("\n");
+  delay(1000);
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void getValues(){
+  temp = bme.readTemperature();
+  pres = bme.readPressure()/100.0f;
+  hum = bme.readHumidity();
+  alt = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  Serial.print("Altitude: "); Serial.println(alt);
+  Serial.print("Pressure: "); Serial.println(pres);
+  Serial.print("Temperature: "); Serial.println(temp);
+  Serial.print("Humidity: "); Serial.println(hum);
 }
